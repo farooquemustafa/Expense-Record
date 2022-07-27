70b15ebf5d868eb250fa8b44bb2c716e449cbf2c
farooquemustafa <farooq_unar@outlook.com>
2022-07-27 08:35:20 +0400

    Cards and useState hook

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from '../UI/Card';
+
+function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title);
+  console.log("Expense Item Evaluated by React!")
+
+  const clickHandler = () => {
+    setTitle("Update!");
+    console.log(title);
+  }
+
+  return (
+    <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
