447c138a7f063925f4dde0d71afa04896c79480b
farooquemustafa <farooq_unar@outlook.com>
2022-08-02 03:07:44 +0400

    file extention update to JSX

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -7,10 +7,7 @@ import Card from '../UI/Card';
 function ExpenseItem(props) {
 
   const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Update!");
-    console.log(title);
-  }
+  
 
   return (
     <Card className="expense-item">
@@ -19,7 +16,6 @@ function ExpenseItem(props) {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
